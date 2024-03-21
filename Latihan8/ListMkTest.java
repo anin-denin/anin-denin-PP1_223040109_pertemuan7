19e892d7fab130f4338e414867be898ac7b18d18
Anin <107341855+anin-denin@users.noreply.github.com>
2024-03-21 15:36:51 +0700

    Add files via upload

---
+++ Latihan8/ListMkTest.java
@@ -0,0 +1,13 @@
+package Latihan8;
+
+public class ListMkTest {
+    public static void main(String[] args) {
+        ListMataKuliah list = new ListMataKuliah();
+
+        list.addHead(new MataKuliah("IF001", "Dasar Pemrograman", 4));
+        list.addHead(new MataKuliah("IF002", "Pemrograman Web", 3));
+        list.displayElement();
+    }
+}
+
+
